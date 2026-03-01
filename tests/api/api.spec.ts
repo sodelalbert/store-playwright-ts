d5ebd4f72e1d00438b712e0f3e7ff5cf0397ef47
Albert Sodel <sodelalbert@gmail.com>
2026-03-01 08:12:03 +0100

    api test update

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,7 +1,8 @@
-import { expect, test } from "../e2e/base-test";
+// TODO: change to base-test after implementing API tests in a separate file
+import { expect, test } from "../e2e/base-test"; 
 
-test("API test", async ({ httpClient }) => {
-  const response = await httpClient.get("/search?q=laptop");
+test("API test", async ({ httpClient: api }) => {
+  const response = await api.get("/search?q=laptop");
   const status = await response.status();
   const text = await response.text();
 
